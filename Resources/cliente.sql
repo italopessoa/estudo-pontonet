dd84ca187dbb40acb2c0acbf7a7d38c16380c3f4
italoneypessoa <italoneypessoa@gmail.com>
2013-05-03 01:08:49 +0000

    alterado tipo de dados de emailcliente

--- Resources/cliente.sql
+++ Resources/cliente.sql
@@ -1,4 +1,4 @@
-﻿-- Table: cliente
+ -- Table: cliente
 
 -- DROP TABLE cliente;
 
@@ -7,7 +7,7 @@ CREATE TABLE cliente
   "IdCliente" serial NOT NULL,
   "NomeCliente" character varying NOT NULL,
   "DtNascimento" date NOT NULL,
-  "EmailCliente" character(1),
+  "EmailCliente" character varying,
   "TelefoneCliente" character varying,
   "DtCadastro" date NOT NULL,
   CONSTRAINT cliente_pk PRIMARY KEY ("IdCliente")
