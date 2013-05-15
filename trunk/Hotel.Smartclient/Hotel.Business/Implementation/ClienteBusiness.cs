59aec202287e5ad7273424ab7490c0d7112f830a
italoneypessoa <italoneypessoa@62b6855c-0dce-2281-36a8-d0688f7ad9e1>
2013-05-15 12:38:22 +0000

    Adicionado summary em Cliente e Quarto

--- trunk/Hotel.Smartclient/Hotel.Business/Implementation/ClienteBusiness.cs
+++ trunk/Hotel.Smartclient/Hotel.Business/Implementation/ClienteBusiness.cs
@@ -20,26 +20,41 @@ namespace Hotel.Business.Implementation
 
         #region IClienteBusiness Members
 
+        /// <summary>
+        /// <see cref="Hotel.Business.IClienteBusiness.InserirCliente"/>
+        /// </summary>
         public void InsertCliente(cliente novoCliente)
         {
             this.clienteData.InsertCliente(novoCliente);   
         }
 
+        /// <summary>
+        /// <see cref="Hotel.Business.IClienteBusiness.RemoveCliente"/>
+        /// </summary>
         public void RemoveCliente(cliente cliente)
         {
             this.clienteData.RemoveCliente(cliente);
         }
 
+        /// <summary>
+        /// <see cref="Hotel.Business.IClienteBusiness.UpdateCliente"/>
+        /// </summary>
         public void UpdateCliente(cliente cliente)
         {
             this.clienteData.UpdateCliente(cliente);
         }
 
+        /// <summary>
+        /// <see cref="Hotel.Business.IClienteBusiness.SelectClientesByNome"/>
+        /// </summary>
         public IList<cliente> SelectClientesByNome(string nomeCliente)
         {
             return this.clienteData.SelectClientesByNome(nomeCliente);
         }
 
+        /// <summary>
+        /// <see cref="Hotel.Business.IClienteBusiness.SelectClientes"/>
+        /// </summary>
         public IList<cliente> SelectClientes()
         {
             return this.clienteData.SelectClientes();
--- trunk/Hotel.Smartclient/Hotel.Data/IClienteData.cs
+++ trunk/Hotel.Smartclient/Hotel.Data/IClienteData.cs
@@ -8,14 +8,35 @@ namespace Hotel.Data
 {
     public interface IClienteData
     {
+        /// <summary>
+        /// Método para inserir novo cliente.
+        /// </summary>
+        /// <param name="novoCliente">Novo Cliente. <see cref="Hotel.Entity.HotelModel.Designer.cs"/> </param>
         void InsertCliente(cliente novoCliente);
 
+        /// <summary>
+        /// Método para remover um cliente.
+        /// </summary>
+        /// <param name="cliente">Cliente a ser removido. <see cref="Hotel.Entity.HotelModel.Designer.cs"/> </param>
         void RemoveCliente(cliente cliente);
 
+        /// <summary>
+        /// Atualizar dados do cliente.
+        /// </summary>
+        /// <param name="cliente">Cliente a ser atualizado. <see cref="Hotel.Entity.HotelModel.Designer.cs"/> </param>
         void UpdateCliente(cliente cliente);
 
+        /// <summary>
+        ///  Selecionar clientes por nome.
+        /// </summary>
+        /// <param name="nomeCliente">Nome do cliente ou parte dele</param>
+        /// <returns>Lista de Clientes <see cref="Hotel.Entity.HotelModel.Designer.cs"/> </returns>
         IList<cliente> SelectClientesByNome(string nomeCliente);
 
+        /// <summary>
+        ///  Selecionar todos os Clientes.
+        /// </summary>
+        /// <returns>Lista de Clientes <see cref="Hotel.Entity.HotelModel.Designer.cs"/> </returns>
         IList<cliente> SelectClientes();
     }
 }
